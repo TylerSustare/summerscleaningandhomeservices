a85f989d9e544f565f524149fd6423ceb83d2133
G. Tyler Sustare <tylersustare@gmail.com>
2019-01-22 21:41:45 -0800

    add copy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ class Footer extends React.Component {
         return (
             <section id="footer">
                 <ul className="icons">
-                    <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                    <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
-                    <li><a href="#" className="icon alt fa-envelope"><span className="label">Email</span></a></li>
+                    <li><a href="https://www.facebook.com/summerscleaningandhomeservices/" target="_blank" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
+                    <li><a href="#" target="_blank" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
+                    <li><a href="mailto:summerscleaningno.1@gmail.com" className="icon alt fa-envelope"><span className="label">Email</span></a></li>
                 </ul>
             </section>
         )
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,12 @@ import { Carousel } from 'react-responsive-carousel';
 
 import Layout from '../components/layout';
 
+import car1 from '../assets/images/dirty-oven.jpeg';
+import car2 from '../assets/images/clean-oven.jpeg';
+import car3 from '../assets/images/dirty-sink.jpeg';
+import car4 from '../assets/images/clean-sink.jpeg';
+import car5 from '../assets/images/dirty-stove.jpeg';
+import car6 from '../assets/images/clean-stove.jpeg';
 import pic01 from '../assets/images/pic01.jpg'
 import pic02 from '../assets/images/pic02.jpg'
 import pic03 from '../assets/images/pic03.jpg'
@@ -33,41 +39,55 @@ class Homepage extends React.Component {
                     <div className="grid-wrapper">
                         <div className="col-12">
                             {
-                                L === 0
-                                    ?
-                                    <span className="image fit"><img src={LogoA} alt="" /></span>
-                                    :
-                                    <span className="image fit"><img src={LogoB} alt="" /></span>
-
+                                <span className="centre"><img src={LogoA} alt="logo" height="150px" /></span>
                             }
-                            <Carousel
-                                autoPlay={true}
-                                interval={2500}
-                                infiniteLoop={true}
-                                dynamicHeight={true}
-                                showArrows={false}
-                            >
-                                <div>
-                                    <img src="https://picsum.photos/2000/1000/?random" />
-                                </div>
-                                <div>
-                                    <img src="https://picsum.photos/2000/1000/?random" />
-                                </div>
-                                <div>
-                                    <img src="https://picsum.photos/2000/1000/?random" />
-                                </div>
-                            </Carousel>
+                            <div className="centre">
+                                <Carousel
+                                    autoPlay={true}
+                                    interval={1500}
+                                    infiniteLoop={true}
+                                    dynamicHeight={true}
+                                    showArrows={false}
+                                    width="50vw"
+                                >
+                                    <div className="rotate">
+                                        <img src={car1} />
+                                    </div>
+                                    <div className="rotate">
+                                        <img src={car2} />
+                                    </div>
+                                    <div className="rotate">
+                                        <img src={car3} />
+                                    </div>
+                                    <div className="rotate">
+                                        <img src={car4} />
+                                    </div>
+                                    <div className="rotate">
+                                        <img src={car5} />
+                                    </div>
+                                    <div className="rotate">
+                                        <img src={car6} />
+                                    </div>
+                                </Carousel>
+                            </div>
                         </div>
                     </div>
                 </section>
+
+                <section id="four" className="main style2 special">
+                    <header className="minor">
+                        <h3> 541-815-9104 <br /> summerscleaningno.1@gmail.com</h3>
+                    </header>
+                </section>
+
                 <section id="one" className="main style1">
                     <div className="grid-wrapper">
                         <div className="col-6">
                             <header className="major">
-                                <h2>Lorem ipsum dolor adipiscing<br />
-                                    amet dolor consequat</h2>
+                                <h2>About Us</h2>
                             </header>
-                            <p>Adipiscing a commodo ante nunc accumsan et interdum mi ante adipiscing. A nunc lobortis non nisl amet vis sed volutpat aclacus nascetur ac non. Lorem curae et ante amet sapien sed tempus adipiscing id accumsan.</p>
+                            <p>Summer's Cleaning and Home Services is a small family owned and operated business. We have been in business here in Central Oregon since April of 2006. You will find that our meticulous attention to detail if far above the rest! Home cleaning is an extremely  personal business, we understand that and we are committed to working together with trust integrity and professionalism. When you call Summer's Cleaning and Home Services you will speak directly with the owners, Summer or Jason,we will speak with you personally to design a customized cleaning service based on YOUR needs, time, and schedule.
+</p>
                         </div>
                         <div className="col-6">
                             <span className="image fit"><img src={pic01} alt="" /></span>
@@ -82,12 +102,9 @@ class Homepage extends React.Component {
                         </div>
                         <div className="col-6">
                             <header className="major">
-                                <h2>Lorem ipsum dolor adipiscing<br />
-                                    amet dolor consequat</h2>
+                                <h2>Free Estimates!!!</h2>
                             </header>
-                            <p>Adipiscing a commodo ante nunc accumsan interdum mi ante adipiscing. A nunc lobortis non nisl amet vis volutpat aclacus nascetur ac non. Lorem curae eu ante amet sapien in tempus ac. Adipiscing id accumsan adipiscing ipsum.</p>
-                            <p>Blandit faucibus proin. Ac aliquam integer adipiscing enim non praesent vis commodo nunc phasellus cubilia ac risus accumsan. Accumsan blandit. Lobortis phasellus non lobortis dit varius mi varius accumsan lobortis. Blandit ante aliquam lacinia lorem lobortis semper morbi col faucibus vitae integer placerat accumsan orci eu mi odio tempus adipiscing adipiscing adipiscing curae consequat feugiat etiam dolore.</p>
-                            <p>Adipiscing a commodo ante nunc accumsan interdum mi ante adipiscing. A nunc lobortis non nisl amet vis volutpat aclacus nascetur ac non. Lorem curae eu ante amet sapien in tempus ac. Adipiscing id accumsan adipiscing ipsum.</p>
+                            <p>At Summer's Cleaning and Home Services you are not just another number, you become part of our family. We treat you and your home how we would want to be treated and how we would want our home cared for. With hard work, honesty, and a level of service that surpasses all of our competitors standards we have managed to become a number one family owned industry leader. We are not your "typical" cleaning service. We meet all of your home care needs by offering Construction Cleans, Deep Cleans, Move Out/In cleans, Monthly cleans, bi-weekly cleans, weekly cleans, vacation rentals, and  air bnb rentals, as well as offering handyman services from fixing a leaky pipe to installing new blinds. We also offer snow removal, gutter cleaning, security checks you name it! If you have a need we can meet it! We become your eyes and ears for your home when you are not able to. Need a service we don't provide? We will work with our trusted business partner we have established over the years and get you taken care of. One call one bill!</p>
                         </div>
                     </div>
                 </section>
@@ -96,25 +113,25 @@ class Homepage extends React.Component {
                     <div className="grid-wrapper">
                         <div className="col-12">
                             <header className="major">
-                                <h2>Adipiscing amet consequat</h2>
+                                <h2>Testimonials</h2>
                             </header>
-                            <p>Ante nunc accumsan et aclacus nascetur ac ante amet sapien sed.</p>
+                            <p>See what people are saying about Summers Cleaning and Home Services.</p>
                         </div>
 
                         <div className="col-4">
                             <span className="image fit"><img src={pic02} alt="" /></span>
-                            <h3>Magna feugiat lorem</h3>
-                            <p>Adipiscing a commodo ante nunc magna lorem et interdum mi ante nunc lobortis non amet vis sed volutpat et nascetur.</p>
+                            <h3>Dacyl Winterbottom</h3>
+                            <p>Came home after a 12 hour shift to a sparkling clean house!! Summer and her team have made my home shine!! their attention to detail is impeccable!! Will be using her services again!! Highly recommend!!</p>
                         </div>
                         <div className="col-4">
                             <span className="image fit"><img src={pic03} alt="" /></span>
-                            <h3>Magna feugiat lorem</h3>
-                            <p>Adipiscing a commodo ante nunc magna lorem et interdum mi ante nunc lobortis non amet vis sed volutpat et nascetur.</p>
+                            <h3>Kendra Jarrett <br />Mom and Owner of Stiletto Salon</h3>
+                            <p>I love coming home to a clean house! Working and being a new mom makes for a busy life and cleaning in my spare time is not something I enjoy doing. Summer's Cleaning has done a fantastic job cleaning my home for the last two years! They are trustworthy and consistent. Their service is worth every penny!! Let the professionals do the work!!</p>
                         </div>
                         <div className="col-4">
                             <span className="image fit"><img src={pic04} alt="" /></span>
-                            <h3>Magna feugiat lorem</h3>
-                            <p>Adipiscing a commodo ante nunc magna lorem et interdum mi ante nunc lobortis non amet vis sed volutpat et nascetur.</p>
+                            <h3>Shona Lyster <br />Client since 2015</h3>
+                            <p>Summer and Jason do a great job cleaning and quality checking their work! I trust them and have recommended their services to friends. They are hard workers who take pride in what they do. I wouldn't use anyone else!</p>
                         </div>
 
                     </div>
@@ -123,9 +140,9 @@ class Homepage extends React.Component {
                 <section id="four" className="main style2 special">
                     <div className="container">
                         <header className="major">
-                            <h2>Ipsum feugiat consequat?</h2>
+                            <h2>Contact Us for a Free Quote</h2>
                         </header>
-                        <p>Sed lacus nascetur ac ante amet sapien.</p>
+                        <p> 541-815-9104 <br /> summerscleaningno.1@gmail.com</p>
                     </div>
                 </section>
             </Layout>
